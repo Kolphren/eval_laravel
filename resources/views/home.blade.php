88f6175ebaeb28119fc9d3e214a0fde8f2bfe92c
Lionnel Traverse <tlionnel@gmail.com>
2021-03-12 15:58:00 +0100

    add css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'BD lovers' )
 
 @section ('content')
-    <h1>Bienvenue BD lovers ! </h1>
+    <h1>Bienvenue chez BD lovers ! </h1>
     <div class="text">
         <p>La plus petite base de données de personnages et dessinateurs de bandes dessinées du monde !</p>
     </div>
